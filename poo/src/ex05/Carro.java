78eeba9e7130a8e14c3a47b8771507a645c6d711
Camila <alvescamila87@gmail.com>
2024-04-15 22:59:49 -0300

    cria classe abstrata, construtor, getters e setters de veiculo + heranca com carro e construtor super

---
+++ poo/src/ex05/Carro.java
@@ -0,0 +1,24 @@
+package ex05;
+
+public class Carro extends Veiculo{
+
+    private int numeroDePortas;
+    public Carro(String marca, String modelo, int ano, int numeroDePortas){
+        super(marca, modelo, ano);
+        this.numeroDePortas = numeroDePortas;
+    }
+
+    public int getNumeroDePortas(){
+        return numeroDePortas;
+    }
+
+    public void setNumeroDePortas(int numeroDePortas){
+        this.numeroDePortas = numeroDePortas;
+    }
+    @Override
+    void display() {
+        System.out.println("INFORMAÇÕES DO CARRO: ");
+        System.out.println("------------------------------------");
+        System.out.printf("Marca: " + getMarca() + "\nModelo: " + getModelo() + "\nAno: " + getAno() + "\nPortas: " +getNumeroDePortas());
+    }
+}
---
+++ poo/src/ex05/TesteCarro.java
@@ -0,0 +1,9 @@
+package ex05;
+
+public class TesteCarro {
+    public static void main(String[] args) {
+
+        Carro c01 = new Carro("BMW", "X6", 2024, 4);
+        c01.display();
+    }
+}
