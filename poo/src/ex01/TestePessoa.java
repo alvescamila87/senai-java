9e059c45dd8d8efa39f12b22128771dfc71e43f2
Camila Alves <alvescamila87@gmail.com>
2024-04-15 21:33:35 -0300

    cria classe pessoa, atributos e método, cria a classe teste pessoa para instanciar o objeto

---
+++ poo/src/ex01/TestePessoa.java
@@ -0,0 +1,11 @@
+package ex01;
+
+public class TestePessoa {
+    public static void main(String[] args) {
+
+        Pessoa pessoa01 = new Pessoa("Princesa Isabel", 40, 1.60);
+
+        pessoa01.falar();
+
+    }
+}
